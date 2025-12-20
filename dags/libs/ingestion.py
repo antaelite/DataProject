7506f57644bfff8f7128d4e9380fd3696d14e7b5
MenphisNono <165615366+MenphisNono@users.noreply.github.com>
2025-12-21 00:32:49 +0100

    Modification de ingestion.py, ajout du traitement des données velov dans wrangling.py et écriture du dag.

--- dags/libs/ingestion.py
+++ dags/libs/ingestion.py
@@ -1,77 +1,97 @@
 import requests
 import csv
+import os
 
-def get_velov_data(output_folder, limit, desired_count):
+def get_velov_data(output_path, api_url=None, limit=100, desired_count=2000):
     """
-    Fetch Vélo'v bike station data and save it to a CSV file.
+    Fetch Vélo'v bike station data and save it to a specific CSV path.
+    Args:
+        output_path (str): Chemin complet du fichier de sortie (ex: /data/landing/file.csv)
+        api_url (str): URL de l'API (optionnel, sinon utilise la valeur par défaut)
+        limit (int): Pagination
+        desired_count (int): Nombre max d'éléments à récupérer
     """
-    fields_names = ["station_id", "name", "address", "district", "position_lat", "position_lng",
-                    "bike_stands","available_bikes", "status", "last_update_date"]
 
-    velov_data = fetch_velov_v_data(limit, desired_count)
-    output_path = output_folder + f"/velov_data.csv"
-    with open(output_path, mode='w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields_names)
-        writer.writeheader()
-        
-        for station in velov_data:
-            properties = station.get('properties', {})
-            row = {
-                "station_id": properties.get('number'),
-                "name": properties.get('name'),
-                "address": properties.get('address'),
-                "district": properties.get('commune'),
-                "position_lat": properties.get('lat'),
-                "position_lng": properties.get('lng'),
-                "bike_stands": properties.get('bike_stands'),
-                "available_bikes": properties.get('available_bikes'),
-                "status": properties.get('status'),
-                "last_update_date": properties.get('last_update')
-            }
-            writer.writerow(row)
-    return True
+    output_folder = os.path.dirname(output_path)
+    if output_folder: # Si le chemin n'est pas juste "velov.csv"
+        os.makedirs(output_folder, exist_ok=True)
 
-def fetch_velov_v_data(limit=100, desired_count=1000):
-    """
-    Fetch Vélo'v bike station data from the Grand Lyon API.
-    The API uses pagination with the 'startIndex' parameter.
-    """
-    base_url = "https://data.grandlyon.com/geoserver/ogc/features/v1/collections/metropole-de-lyon:jcd_jcdecaux.jcdvelov/items"
+    #Configuration de l'API
+    if api_url is None:
+        api_url = "https://data.grandlyon.com/geoserver/ogc/features/v1/collections/metropole-de-lyon:jcd_jcdecaux.jcdvelov/items"
+    
     all_data = []
     start_index = 0
-    nb_requests = 0
-    
+
+    print(f"Début de l'ingestion vers le fichier : {output_path}...")
+    print(f"URL cible : {api_url}")
+
+    # Boucle de pagination
     while len(all_data) < desired_count:
         params = {
             "limit": limit,
             "startIndex": start_index,
             "f": "json"
         }
-        response = requests.get(base_url, params=params)
-        if response.status_code != 200:
-            print(f"Error fetching data: {response.status_code}")
+        try:
+            response = requests.get(api_url, params=params, timeout=10)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Erreur API : {e}")
             break
+
         data = response.json()
         items = data.get('features', [])
+
+        if not items:
+            break
+
         all_data.extend(items)
-        nb_requests += 1
+        print(f"Récupéré : {len(all_data)} stations...")
+        
         if len(items) < limit:
             break
 
         start_index += limit
 
-    return all_data[:desired_count]
+    # Écriture du CSV     
+    final_data = all_data[:desired_count]
+    
+    fields_names = ["station_id", "name", "address", "district", "lat", "lng", 
+                    "bike_stands", "available_bikes", "status", "last_update"]
 
-def main():
-    output_folder = "../data/landing"
-    limit = 100
-    desired_count = 2000
+    with open(output_path, mode='w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields_names)
+        writer.writeheader()
 
-    success = get_velov_data(output_folder, limit, desired_count)
-    if success:
-        print("Vélo'v data ingestion completed successfully.")
-    else:
-        print("Vélo'v data ingestion failed.")
+        for station in final_data:
+            properties = station.get('properties', {})
+            row = {
+                "station_id": properties.get('number'),
+                "name": properties.get('name'),
+                "address": properties.get('address'),
+                "district": properties.get('commune'),
+                "lat": properties.get('lat'),
+                "lng": properties.get('lng'),
+                "bike_stands": properties.get('bike_stands'),
+                "available_bikes": properties.get('available_bikes'),
+                "status": properties.get('status'),
+                "last_update": properties.get('last_update')
+            }
+            writer.writerow(row)
+    
+    print(f"Ingestion terminée. Fichier sauvegardé dans : {output_path}")
+
+    return output_path
 
 if __name__ == "__main__":
-    main()
+    fichier_test_local = "../../data/landing/velov_raw.csv"
+    
+    print("TEST manuel de la fonction d'ingestion Vélo'v...")
+    try:
+        chemin_fichier = get_velov_data(output_path=fichier_test_local, limit=10, desired_count=20)
+        print(f"TEST RÉUSSI ! Fichier créé : {chemin_fichier}")
+    except Exception as e:
+        print(f"TEST ÉCHOUÉ.")
+        print(f"Raison de l'erreur : {e}")
+
--- dags/libs/wrangling.py
+++ dags/libs/wrangling.py
@@ -1,4 +1,105 @@
-import pandas as pd
+import pandas as pd 
+import os
+
+# Configuration des chemins (BASE_DIR à ajuster selon si on est en local ou Docker)
+# En local : os.path.join("..", "..", "data")
+# En Docker : "/opt/airflow/data"
+BASE_DIR = os.path.join("..", "..", "data")
+
+def accidents_cleaning (input_path, output_path):
+    """
+    Nettoie les données d'accidents.
+    """
+    print(f"--- Traitement Accidents : {input_path} ---")
+
+    if not os.path.exists(input_path):
+        print(f"SKIPPING: Fichier introuvable {input_path}. Lancer l'ingestion d'abord.")
+        return
+    
+    output_dir = os.path.dirname(output_path)
+    os.makedirs(output_dir, exist_ok=True)
+
+    print("Lecture du fichier brut...")
+    df = pd.read_csv(input_path)
+    if "dep" in df.columns:
+        df = df[df["dep"].astype(str) == "69"]
+    else:
+        print("La colonne 'dep' n'existe pas dans ce dataset ! Vérifie le nom exact.")
+
+    df["lat"] = pd.to_numeric(df["lat"], errors="coerce")
+    df["long"] = pd.to_numeric(df["long"], errors="coerce")
+    df = df.dropna(subset=["lat", "long"])
+    df = df[(df["lat"] != 0) & (df["long"] != 0)]
+
+    df = df.drop_duplicates()
+
+    keep_columns = []
+    cols_to_keep = ["Num_Acc", "grav", "date", "an", "mois", "jour", "hrmn", "com", "lat", "long"]
+    keep_columns = [col for col in df.columns if col in cols_to_keep]
+
+    df = df[keep_columns]
+
+    output_path = os.path.join(output_dir, "accidents_clean_lyon.csv")
+    df.to_csv(output_path, index=False, encoding="utf-8")
+    print(f" Fichier nettoyé enregistré dans : {output_path}")
+    print(f"Nombre de lignes finales : {len(df)}")
+
+
+def clean_velov_data(input_path, output_path):
+    """
+    Nettoie les données Vélo'v.
+    """
+    print(f"--- Traitement Vélo'v : {input_path} ---")
+    
+    if not os.path.exists(input_path):
+        print(f"ERREUR : Fichier introuvable {input_path}. Lancer l'ingestion d'abord.")
+        return
+
+    df = pd.read_csv(input_path, sep=",") # Vérifie si c'est , ou ;
+
+    # 1. Types
+    df['lat'] = pd.to_numeric(df['lat'], errors='coerce')
+    df['lng'] = pd.to_numeric(df['lng'], errors='coerce')
+    df['available_bikes'] = pd.to_numeric(df['available_bikes'], errors='coerce').fillna(0).astype(int)
+    
+    # 2. Filtres
+    df = df.dropna(subset=['lat', 'lng'])
+    
+    if 'status' in df.columns:
+        df = df[df['status'] == 'OPEN']
+
+    # 3. Dates
+    if 'last_update' in df.columns:
+        df['last_update'] = pd.to_datetime(df['last_update'], errors='coerce')
+
+    # 4. Sauvegarde
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+    print(f"-> Sauvegardé : {output_path} ({len(df)} stations)")
 
 
 
+if __name__ == "__main__":
+    print("Démarrage du script de Wrangling...")
+
+    # 1. Définir les chemins d'entrée (Landing)
+    accidents_raw = os.path.join(BASE_DIR, "landing", "accidentsVelo.csv")
+    velov_raw = os.path.join(BASE_DIR, "landing", "velov_raw.csv")
+
+    # 2. Définir les chemins de sortie (Staging)
+    accidents_clean_path = os.path.join(BASE_DIR, "staging", "accidents_clean_lyon.csv")
+    velov_clean_path = os.path.join(BASE_DIR, "staging", "velov_clean.csv")
+
+    # 3. Exécuter les fonctions séquentiellement
+    # On gère les exceptions pour qu'une erreur sur l'un ne bloque pas forcément l'autre
+    try:
+        accidents_cleaning(accidents_raw, accidents_clean_path)
+    except Exception as e:
+        print(f"Erreur sur Accidents: {e}")
+
+    try:
+        clean_velov_data(velov_raw, velov_clean_path)
+    except Exception as e:
+        print(f"Erreur sur Vélo'v: {e}")
+
+    print("Fin du Wrangling.")
